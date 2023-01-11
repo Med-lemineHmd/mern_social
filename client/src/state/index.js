11cdf8c63aa9d6d38afdacb4d2d1d0dc0d618634
Med-lemineHmd <leminlemin00@gmail.com>
2023-01-11 04:01:42 +0100

    Redux Setup

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -4,10 +4,45 @@ const initialState = {
   mode: "light",
   user: null,
   token: null,
-  post: [],
+  posts: [],
 };
 
 export const authSlice = createSlice({
-    name: "auth",
-    initialState,
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.log("user friends non-existent :(");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const udpdatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post._id) return action.payload.post;
+        return post;
+      });
+      state.posts = udpdatedPosts;
+    },
+  },
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+  authSlice.actions;
+
+export default authSlice.reducer;
