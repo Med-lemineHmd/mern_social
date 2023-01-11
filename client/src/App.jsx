e19db76effb2e2fe82ad948e085fde2639b713b1
Med-lemineHmd <leminlemin00@gmail.com>
2023-01-11 17:22:45 +0100

    DarkMode - Color - Theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,11 @@ import {
 } from "react-router-dom";
 import { HomePage, LoginPage, ProfilePage, Navbar, NotFound } from "./pages";
 import RootLayout from "./layouts/RootLayout";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -20,7 +25,15 @@ const router = createBrowserRouter(
 );
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import authSlice from "./state";
+import authReducer from "./state";
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import {
