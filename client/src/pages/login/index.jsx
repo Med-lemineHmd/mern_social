531e67dfd73470c19da3f00a662995cd07c4ede7
Med-lemineHmd <leminlemin00@gmail.com>
2023-01-13 04:14:21 +0100

    Login & Register - Form

--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import {
+  Box,
+  Typography,
+  useTheme,
+  useMediaQuery,
+  Avatar,
+} from "@mui/material";
+import VerifiedIcon from "@mui/icons-material/Verified";
+import brand from "../../assets/brand.png";
+import Form from "./Form";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Avatar
+          alt="Brand"
+          src={brand}
+          sx={{ padding: "3px", width: "50px", height: "50px" }}
+        />
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem, 1.6rem, 2rem)"
+          color="primary"
+        >
+          MoSocial
+        </Typography>
+      </Box>
+
+      {/* FORM BOX */}
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography
+          fontWeight="500"
+          display="flex"
+          alignItems="center"
+          variant="h5"
+          sx={{ mb: "1.5rem" }}
+        >
+          Welcome to MoSocial, the Social Media for Sociopaths{" "}
+          <VerifiedIcon sx={{ color: "yellow" }} />
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
